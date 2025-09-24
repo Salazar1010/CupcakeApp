7445efb80d2f35f260704e5b52168b366368a435
Jean Salazar <sagrajean38@gmail.com>
2025-09-24 16:11:09 -0500

    Creando un botón reutilizable con Compose (SelectQuantityButton)

--- app/src/main/java/com/example/cupcakeapp/ui/theme/StartOrderScreen.kt
+++ app/src/main/java/com/example/cupcakeapp/ui/theme/StartOrderScreen.kt
@@ -1,24 +1,35 @@
 package com.example.cupcakeapp.ui.theme
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cupcakeapp.ui.theme.CupcakeAppTheme
-import androidx.compose.foundation.layout.fillMaxWidth
+
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.dimensionResource
 import com.example.cupcakeapp.R
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+
+
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Text
-import androidx.compose.material3.MaterialTheme
+
 import androidx.compose.ui.res.stringResource
+
+
 
 @Composable
 fun StartOrderScreen(
@@ -56,6 +67,21 @@ fun StartOrderScreen(
 
         }
 
+    }
+}
+
+
+@Composable
+fun SelectQuantityButton(
+    @StringRes labelResourceId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.widthIn(min = 250.dp)
+    ) {
+        Text(stringResource(labelResourceId))
     }
 }
 
